afe10a577e949bde0014c34b279a0218663b80c3
simu13 <simrandhillon5486@gmail.com>
2019-09-27 18:16:28 -0400

    Connection made.

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -3,17 +3,25 @@
  * Project: Calculator
  * Name: Simranjeet Singh Dhillon
  * StudentID: 301093914
- * Version: V1
+ * Version: V2 - Connection made
  */
 
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelUp: UILabel!
+    @IBOutlet weak var labelDown: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        labelUp.text = "9"
+        labelDown.text = "+"
+        
         
     }
     
@@ -22,5 +30,12 @@ class ViewController: UIViewController {
     }
 
 
+  
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+        
+        
+    }
+    
 }
 
