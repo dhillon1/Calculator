6f9436eaae012c5497658b680e043894171b3abe
simu13 <simrandhillon5486@gmail.com>
2019-09-28 11:21:33 -0400

    Logic for . button created.

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -3,7 +3,7 @@
  * Project: Calculator
  * Name: Simranjeet Singh Dhillon
  * StudentID: 301093914
- * Version: V4 - Logic for C button created
+ * Version: V5 - Logic for . button created
  */
 
 
@@ -15,10 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelDown: UILabel!
     private var m_operand = "0"
     private var m_operator = ""
-    
-    
-    
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -38,12 +35,14 @@ class ViewController: UIViewController {
         
         switch (calculatorButton) {
         case ".":
-            if(m_operand == "0"){
+            if(!m_operand.contains(".")){
             m_operand += calculatorButton!
                 labelDown.text = m_operand
             }
+            
             break
         case "+":
+            
             break
         case"=":
             break
